28788b8ab2c3ef1bb69cf9e3d1be40d56365ba6f
Muhammad Diki <d.darmawan2111@gmail.com>
2023-10-13 17:22:11 +0700

    feat(bloc-builder): bloc builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_part1/page/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,85 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
-    );
-  }
-}
-
-class CounterCubit extends Cubit<int> {
-  int initialData;
-
-  CounterCubit({this.initialData = 0}) : super(initialData);
-
-  
-  void tambahData() {
-    emit(state + 1);
-  }
-
-  void kurangData() {
-    emit(state - 1);
-  }
-
-  // Observer
-  // perubahann
-  // on error
-
-  @override
-  void onChange(Change<int> change) {
-    super.onChange(change);
-    print(change);
-  }
-
-  @override
-  void onError(Object error, StackTrace stackTrace) {
-    super.onError(error, stackTrace);
-    print(error);
-    print(stackTrace);
-  }
-}
-
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
-
-  CounterCubit myCounter = CounterCubit(initialData: 0);
-
-  @override
-  Widget build(BuildContext context) {
-    print('Rebuild');
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          StreamBuilder(
-            initialData: myCounter.initialData,
-            stream: myCounter.stream,
-            builder: (context, snapshot) {
-              return Center(
-                child: Text(
-                  "${snapshot.data}",
-                  style: const TextStyle(
-                    fontSize: 50,
-                  ),
-                ),
-              );
-            }
-          ),
-          const SizedBox(height: 20,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(onPressed: (){
-                myCounter.kurangData();
-              }, icon: const Icon(Icons.remove)),
-              IconButton(onPressed: (){
-                myCounter.tambahData();
-              }, icon: const Icon(Icons.add)),
-            ],
-          ),
-        ],
-      ),
     );
   }
 }
